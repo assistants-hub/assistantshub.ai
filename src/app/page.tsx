b8e4aea927aca02c2174af7933e3539cfac008f1
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-01-09 21:06:50 -0800

    #4 Adding Prettier and format:fix options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Alert } from "flowbite-react";
+import React from 'react';
+import { Alert } from 'flowbite-react';
 
 export default function Home() {
   return (
-      <div>
-        <Alert
-            color="success">
-          <h3 className="text-lg font-medium text-green-700 dark:text-green-800">
-            Welcome to Assistants Hub!
-          </h3>
-        </Alert>
-      </div>
-  )
+    <div>
+      <Alert color='success'>
+        <h3 className='text-lg font-medium text-green-700 dark:text-green-800'>
+          Welcome to Assistants Hub!
+        </h3>
+      </Alert>
+    </div>
+  );
 }
