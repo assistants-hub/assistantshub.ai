5e2cfb4281a78d877daf0c21055a865f9fa6679b
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-01-10 15:32:27 -0800

    #6 Fixes to runtime logo rendering issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,8 @@ export function Header() {
   return (
     <Navbar fluid rounded>
       <NavbarBrand href="https://assistants-hub.vercel.app">
-        <Image src="/logo.png" className="mr-3 h-6 sm:h-9" alt="Assistants Hub Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Assistants Hub</span>
+        <Image src="/logo.png" alt="Assistants Hub Logo" width="48" height="48" />
+        <span className="pl-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white">Assistants Hub</span>
       </NavbarBrand>
       <div className="flex md:order-2">
         <Button outline gradientDuoTone="purpleToBlue" size="sm">Login</Button>
