722f8df79768d4df948467e3aa434dc8782234d2
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-01-10 21:01:42 -0800

    #6 Add GoogleProvider for authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,17 @@
 import NextAuth from 'next-auth/next';
 import GitHub from 'next-auth/providers/github';
+import GoogleProvider from "next-auth/providers/google";
+
 export const handler = NextAuth({
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
+    })
   ],
   secret: process.env.NEXTAUTH_SECRET,
   theme: {
