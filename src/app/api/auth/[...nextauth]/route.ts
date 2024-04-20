93289d7bce4fb0c9b95b75fd8dc9faef55003c7e
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-04-20 00:23:06 -0700

    #23 Remove BYOK

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,29 @@
 import NextAuth from 'next-auth/next';
 import GitHub from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID : '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ? process.env.GOOGLE_CLIENT_SECRET : '',
+      clientId: process.env.GOOGLE_CLIENT_ID
+        ? process.env.GOOGLE_CLIENT_ID
+        : '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        ? process.env.GOOGLE_CLIENT_SECRET
+        : '',
     }),
     GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID ? process.env.GITHUB_CLIENT_ID : '',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ? process.env.GITHUB_CLIENT_SECRET : '',
+      clientId: process.env.GITHUB_CLIENT_ID
+        ? process.env.GITHUB_CLIENT_ID
+        : '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+        ? process.env.GITHUB_CLIENT_SECRET
+        : '',
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
@@ -25,9 +32,9 @@ export const authOptions = {
     logo: '/logo.png', // Absolute URL to image
   },
   session: {
-    strategy: "jwt"
-  }
-}
+    strategy: 'jwt',
+  },
+};
 
 // @ts-ignore
 export const handler = NextAuth(authOptions);
--- src/app/assistants/GetStarted.tsx
+++ src/app/assistants/GetStarted.tsx
@@ -6,7 +6,11 @@ import { redirect } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import CreateAssistant from '@/app/assistants/CreateAssistant';
 import CreateCredentials from '@/app/assistants/CreateCredentials';
-import { useGetAssistants, useGetCredentials } from '@/app/assistants/client';
+import {
+  setCredentials,
+  useGetAssistants,
+  useGetCredentials,
+} from '@/app/assistants/client';
 import ListAssistants from '@/app/assistants/ListAssistants';
 
 export default function GetStarted() {
@@ -17,22 +21,35 @@ export default function GetStarted() {
       redirect('api/auth/signin');
     },
   });
-  const [credentialCreated, setCredentialCreated] = useState(false);
+  const [blocked, setBlocked] = useState(true);
   const [assistantCreated, setAssistantCreated] = useState(false);
   const { credentialsLoading, credentials, credentialsEmpty } =
     useGetCredentials();
   const { assistantsLoading, assistants, assistantsEmpty } = useGetAssistants();
 
-  useEffect(() => {}, [credentialCreated]);
+  useEffect(() => {
+    if (!credentialsLoading && credentialsEmpty) {
+      setBlocked(true);
+      setCredentials('use-default').then((response) => {
+        // Wait till credentials are created.
+        setBlocked(false);
+      });
+    }
+
+    if (credentials && credentials.length) {
+      setBlocked(false);
+    }
+  }, [credentialsLoading]);
 
   return (
     <>
-      {status === 'loading' || credentialsLoading || assistantsLoading ? (
+      {status === 'loading' ||
+      blocked ||
+      credentialsLoading ||
+      assistantsLoading ? (
         <div className='bg-grey flex h-[calc(100vh-120px)] items-center justify-center '>
           <Spinner />
         </div>
-      ) : credentialsEmpty && !credentialCreated ? (
-        <CreateCredentials setCredentialCreated={setCredentialCreated} />
       ) : assistantsEmpty && !assistantCreated ? (
         <CreateAssistant />
       ) : (
