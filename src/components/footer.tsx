f1845c525aa41b33eddb95063ec4bd48ea368b11
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-05-14 22:51:07 -0700

    #66 Initial fixes to Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,64 @@
+"use client";
+
 import {
   Footer,
   FooterCopyright,
-  FooterLink,
-  FooterLinkGroup,
 } from 'flowbite-react';
 import { BsGithub } from 'react-icons/bs';
 
 export function PageFooter() {
   return (
-    <Footer container>
-      <div className='flex items-end justify-end'>
-        <div className='flex justify-end space-x-3 sm:flex sm:items-center sm:justify-between'>
+    <Footer bgDark>
+      <div className="w-full text-sm">
+        <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
+          <div>
+            <Footer.Title title="Company" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">About Us</Footer.Link>
+              <Footer.Link href="#">Careers</Footer.Link>
+              <Footer.Link href="#">Brand Center</Footer.Link>
+              <Footer.Link href="#">Blog</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="Help center" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="legal" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="/privacy">Privacy Policy</Footer.Link>
+              <Footer.Link href="/terms">Terms &amp; Conditions</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="legal" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="/privacy">Privacy Policy</Footer.Link>
+              <Footer.Link href="/terms">Terms &amp; Conditions</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+        </div>
+        <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between">
           <FooterCopyright
-            href='#'
-            by='Assistants Hub'
+            href="#"
+            by="Assistants Hub LLC"
             year={new Date().getFullYear()}
           />
-          <FooterLinkGroup>
-            <div className='w-full justify-between sm:flex sm:items-center sm:justify-between'>
-              <Footer.Icon
-                href='https://github.com/santthosh/assistants-hub'
-                icon={BsGithub}
-              />
-            </div>
-          </FooterLinkGroup>
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsGithub} />
+          </div>
         </div>
       </div>
     </Footer>
-  );
+    );
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='h-[calc(100vh-115px)] justify-center bg-gray-50 '>
+    <div className='justify-center bg-gray-50 '>
       <div className='grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-5'>
         <div className='flex px-4 py-24 text-left sm:col-span-1 md:col-span-2 lg:col-span-1 lg:py-56'>
           <h1 className='mx-auto mb-4 text-5xl font-normal leading-none tracking-tight xl:text-6xl'>
