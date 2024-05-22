5758f6cdba2821c85d566457cdea858eec54db70
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-05-21 23:49:24 -0700

    Adding background color that matches the theme

--- src/app/utils/assistant.ts
+++ src/app/utils/assistant.ts
@@ -17,6 +17,29 @@ export function getSecondaryColor(assistant: Assistant) {
   return getStyleHash(assistant.id).secondaryColor;
 }
 
+function hexToRgbA(hex, opacity = 1) {
+  let c;
+  if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+    c= hex.substring(1).split('');
+    if(c.length== 3){
+      c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    c= '0x'+c.join('');
+    return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',' + opacity + ')';
+  }
+  throw new Error('Bad Hex');
+}
+
+export function getPrimaryBackgroundColor(assistant: Assistant) {
+  if (assistant.theme && assistant.theme.primaryColor) {
+    return hexToRgbA(assistant.theme.primaryColor, 0.7);
+  }
+
+  return hexToRgbA(getStyleHash(assistant.id)
+    .primaryColor.replace('bg-[', '')
+    .replace(']', ''),0.7);
+}
+
 export function getPrimaryTextColor(assistant: Assistant) {
   if (assistant.theme && assistant.theme.primaryTextColor) {
     return assistant.theme.primaryTextColor;
