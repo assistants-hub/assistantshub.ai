352b787ada3335df15ef2eea3e0e8d2333c78a38
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-06-09 16:42:43 -0700

    81 Fixing the layout on the grid

--- src/app/assistants/ListAssistants.tsx
+++ src/app/assistants/ListAssistants.tsx
@@ -36,7 +36,7 @@ export default function ListAssistants() {
   }, [assistantCreated]);
 
   return (
-    <div className='flex min-h-[calc(100vh-160px)] flex-col p-10 '>
+    <div className='flex min-h-[calc(100vh-160px)] flex-col xs:p-2 sm:p-10'>
       <div>
         <Button
           className='float-right'
@@ -46,8 +46,16 @@ export default function ListAssistants() {
           <HiPlus className='mr-2 h-5 w-5' /> Create Assistant
         </Button>
       </div>
-      <div className='bg-grey flex flex-auto items-start justify-start'>
-        <div className='grid grid-cols-1 gap-4 p-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 xl:gap-16 2xl:grid-cols-4'>
+      <div className='bg-grey m-5 flex flex-auto items-start justify-center'>
+        <div
+          className='4xl:grid-cols-4 grid
+                             grid-cols-1
+                             gap-12
+                             md:grid-cols-1
+                             xl:grid-cols-2
+                             2xl:grid-cols-2
+                             3xl:grid-cols-3'
+        >
           {assistants &&
             assistants.length &&
             assistants.map((assistant) => {
@@ -58,7 +66,7 @@ export default function ListAssistants() {
                       ? assistant.profile
                       : '/images/people/' + getImageHash(assistant.id) + '.jpg'
                   }
-                  className='min-w-max max-w-md'
+                  className='xs:max-w-72 sm:min-w-max sm:max-w-sm'
                   horizontal
                   key={assistant.id}
                 >
@@ -74,13 +82,13 @@ export default function ListAssistants() {
                     <span className='pt-4 text-sm text-gray-500 dark:text-gray-400'>
                       {assistant.description}
                     </span>
-                    <div className='mt-4 flex space-x-3 lg:mt-6'>
+                    <div className='mt-4 grid items-center justify-center gap-2 xs:grid-cols-1 sm:grid-cols-2 lg:mt-6'>
                       <Link href={`/link/${assistant.id}`} target='_blank'>
                         <Button gradientDuoTone='greenToBlue'>
                           <HiChatAlt2 className='mr-1 h-5 w-5' /> Try Assistant
                         </Button>
                       </Link>
-                      <Dropdown label='Go to' color={'light'} placement='top'>
+                      <Dropdown label='Options' color={'light'} placement='top'>
                         <DropdownItem
                           icon={HiChartBar}
                           href={`/assistants/${assistant.id}/analytics`}
