f89ce666bf8331bb7cae652538248587f97803fc
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-06-30 14:32:19 -0700

    #93 Adding the basic dashboard and authentication mechanism

---
+++ src/app/admin/dashboard/AssistantMetrics.tsx
@@ -0,0 +1,63 @@
+import prisma from '@/app/api/utils/prisma';
+import { Card } from 'flowbite-react';
+
+export default function AssistantMetrics() {
+  const getAssistantCount = async function() {
+    let total = await prisma.assistant.aggregate({
+      _count: {
+        id: true
+      }
+    })
+
+    return total._count.id
+  }
+
+  const getThreadCount = async function() {
+    let total = await prisma.thread.aggregate({
+      _count: {
+        id: true
+      }
+    })
+
+    return total._count.id
+  }
+
+  const getMessageCount = async function() {
+    let total = await prisma.message.aggregate({
+      _count: {
+        id: true
+      }
+    })
+
+    return total._count.id
+  }
+
+  return (
+    <div className={'grid xs:grid-flow-row sm:grid-flow-col gap-4'}>
+      <Card className="flex flex-auto">
+        <h5 className="text-lg tracking-tight text-gray-400 dark:text-white">
+          Assistants
+        </h5>
+        <p className="text-7xl text-gray-700 dark:text-gray-400">
+          {getAssistantCount()}
+        </p>
+      </Card>
+      <Card className="flex flex-auto">
+        <h5 className="text-lg tracking-tight text-gray-400 dark:text-white">
+          Threads
+        </h5>
+        <p className="text-7xl text-gray-700 dark:text-gray-400">
+          {getThreadCount()}
+        </p>
+      </Card>
+      <Card className="flex flex-auto">
+        <h5 className="text-lg tracking-tight text-gray-400 dark:text-white">
+          Messages
+        </h5>
+        <p className="text-7xl text-gray-700 dark:text-gray-400">
+          {getMessageCount()}
+        </p>
+      </Card>
+    </div>
+  )
+}
--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import AssistantMetrics from '@/app/admin/dashboard/AssistantMetrics';
+import UserMetrics from '@/app/admin/dashboard/UserMetrics';
 
 export default function Dashboard() {
   return (
@@ -7,6 +9,18 @@ export default function Dashboard() {
       <p className={'pb-4 text-sm text-gray-400'}>
         Analytics on your Assistants Hub instance
       </p>
+      <div className={'pt-4'}>
+        <div className={'pb-4 text-xl text-gray-400'}>
+          Assistant Metrics
+        </div>
+        <AssistantMetrics />
+      </div>
+      <div className={'pt-4'}>
+        <div className={'pb-4 text-xl text-gray-400'}>
+          User Metrics
+        </div>
+        <UserMetrics />
+      </div>
     </div>
   );
 }
